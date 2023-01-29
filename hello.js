c19838c3e99b0db8be1bab9ea9d188c38095088b
tawrat41 <tawratur.rashid@gmail.com>
2023-01-30 00:31:30 +0600

    dot dot

--- hello.js
+++ hello.js
@@ -1,12 +1,56 @@
 // console.log(10)
 
-var egg = 20;
-var val = 5.2219841;
-var val1 = 2.1621;
+// var egg = 20;
+// var val = 5.2219841;
+// var val1 = 2.1621;
 
-var sum = val + val1
-sum = parseInt(sum)
-console.log(sum)
+// var sum = val + val1
+// sum = parseInt(sum)
+// console.log(sum)
 
 
+// var arr = [1,2,3,5,9,10]
 
+// var index = arr.indexOf(9)
+// arr.splice(index, 1);
+// // if (index > -1) {
+// //   arr.splice(index, 1);
+// // }
+
+// console.log(arr)
+
+// arr.splice(index, 1)
+
+
+// const numbers = [1, 2, 3];
+
+// const index = numbers.indexOf(3);
+// if (index > -1) {
+//   numbers.splice(index, 1);
+// }
+
+// console.log(numbers); 
+
+
+// console.log(6>=6)
+
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+
+// var i = 5;
+// for (i < 5; i++) {
+//  console.log(i);
+// }
+
+function movie(){
+
+    return "Din-The day";
+  
+    return "Poran";
+  
+    return "Hawa";
+  
+  }
+  
+  console.log(movie());
